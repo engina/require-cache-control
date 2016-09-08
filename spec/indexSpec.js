5d5022ea7269879c17e6bd1e3534bbb70addbc23
Engin AYDOGAN <engin@bzzzt.biz>
2016-09-08 15:28:17 +0300

    made test node 0.10 compatible

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -2,7 +2,7 @@ const test = require('tape');
 const rcc = require('../');
 
 test('require-cache-control', function(t) {
-  let snapshot = rcc.snapshot();
+  var snapshot = rcc.snapshot();
   t.equal(require('./fixtures/a')(), 0);
   t.equal(require('./fixtures/a')(), 1);
   t.equal(require('./fixtures/a')(), 2);
